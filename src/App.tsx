860ecd0872d31f4e33f4397df769220c8164bddd
Geraldo157 <gsmecanica76@gmail.com>
2023-05-18 19:51:23 -0300

    Finishing Day

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import words from './wordList.json';
 import {HangmanWord} from './HangmanWord';
 import {HangmanKeyboard} from './HangmanKeyboard';
@@ -12,19 +12,32 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
+  const incorrectLetters = guessedLetters.filter(letter => !wordGuess.includes(letter))
+
+  function addGuessesLetter(letter: string){
+    if(guessedLetters.includes(letter)) return
+    setGuessedLetters(currentLetters => [...currentLetters, letter])
+  }
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+      if(!key.match(/^[a-z]$/)) return
+
+      e.preventDefault()
+      addGuessesLetter(key)
+    }
+
+    document.addEventListener("keypress", handler)
+
+    return () =>{
+      document.removeEventListener("keypress", handler)
+    }
+  }, [])
+
   return (
     <div
     className="max-w-[800px] mx-auto flex flex-col gap-8 items-center"
-    /*style={{
-      maxWidth: "800px",
-      display: "flex",
-      flexDirection: "column",
-      gap: "2rem",
-      margin: "0 auto",
-      alignItems: "center",
-      
-    }}*/
   >
     <div style={{ fontSize: "2rem", textAlign: "center" }}>
       Win lose
@@ -32,8 +45,7 @@ function App() {
     <HangmanDrawing numberOfGuesses={incorrectLetters.length}/>
     <HangmanWord guessedLetters = {guessedLetters} wordToGuess={wordGuess}/>
     <div style={{ alignSelf: "stretch" }}>
-      <HangmanKeyboard
-      />
+      <HangmanKeyboard/>
     </div>
   </div>)
 }
