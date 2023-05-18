46b932ec7e5b8c5d24efea64cca7a712450fe513
Geraldo157 <gsmecanica76@gmail.com>
2023-05-18 05:24:24 -0300

    Setting up TailWindCss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
 import words from './wordList.json';
+import './App.css'
 
 function App() {
   const [wordGuess, setWordGuess] = useState(()=>{
     return words[Math.floor(Math.random() * words.length)]
   })
 
-  return (<h1>Hello World</h1>)
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  return (<h1 className="text-red-500">Hello World</h1>)
 }
 
 export default App
